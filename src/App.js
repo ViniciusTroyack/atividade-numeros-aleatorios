de4f7c078c33fafbdc430cdf3f6ce411e727d438
Vinicius Troyack <troyackvinicius@gmail.com>
2021-06-30 16:45:32 -0300

    commitado

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(Math.floor(Math.random() * 100) + 1);
+
+  function randomNumber() {
+    setNumber(Math.floor(Math.random() * 100) + 1);
+
+    let btn = document.querySelector(".btn");
+    let r = Math.floor(Math.random() * 255),
+      g = Math.floor(Math.random() * 255),
+      b = Math.floor(Math.random() * 255);
+    btn.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Numero: {number}</div>
+
+        <button className="btn" onClick={randomNumber}>
+          Criar novo número
+        </button>
       </header>
     </div>
   );
